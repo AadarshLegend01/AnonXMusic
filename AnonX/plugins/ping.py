9c02504b79405d167bdc779e08b87a51418a4a7b
꧁༄ᶦᶰᵈ᭄ꪁᴅꪁʀʂʜ࿐ <91910159+AadarshLegend01@users.noreply.github.com>
2023-07-01 12:11:05 +0530

    Update ping.py
    Signed-off-by: ꧁༄ᶦᶰᵈ᭄ꪁᴅꪁʀʂʜ࿐ <91910159+AadarshLegend01@users.noreply.github.com>

--- AnonX/plugins/ping.py
+++ AnonX/plugins/ping.py
@@ -12,24 +12,24 @@ from AnonX.utils.decorators.language import language
 from AnonX.utils.inline.play import close_keyboard
 
 ### Commands
-PING_COMMAND = get_command("PING_COMMAND")
+PING_COMMAND = get_command("MPING_COMMAND")
 
 
 @app.on_message(
-    filters.command(PING_COMMAND)
+    filters.command(MPING_COMMAND)
 )
 @language
 async def ping_com(client, message: Message, _):
     response = await message.reply_photo(
         photo=PING_IMG_URL,
-        caption=_["ping_1"],
+        caption=_["mping_1"],
     )
     start = datetime.now()
     pytgping = await Anon.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(
+        _["mping_2"].format(
             resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
         ),
         reply_markup=close_keyboard
